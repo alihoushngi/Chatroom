cc6a7b254bbf8e8ead7aeb16ea6e7b8161523151
alihoushngi <alihoushngi@gmail.com>
2023-05-04 16:37:37 +0430

    header created

--- src/components/Shared/Header/Header.tsx
+++ src/components/Shared/Header/Header.tsx
@@ -1,8 +1,31 @@
 import React from "react";
-import { StyledHeader } from "./StyledHeader";
+import {
+  StyledHeader,
+  StyledProfile,
+  StyledLogoWrapper,
+  StyledInfoWrapper,
+  StyledNameTag,
+  StyledTagline,
+} from "./StyledHeader";
+import Image from "next/image";
+import avatar from "@images/avatar.jpeg";
+import logo from "@images/logo.png";
 
 const Header = () => {
-	return <StyledHeader></StyledHeader>;
+  return (
+    <StyledHeader>
+      <StyledProfile>
+        <Image alt="avatar" src={avatar} />
+        <StyledInfoWrapper>
+          <StyledNameTag>Ali Houshangi</StyledNameTag>
+          <StyledTagline>Frontend Developer | NextJS</StyledTagline>
+        </StyledInfoWrapper>
+      </StyledProfile>
+      <StyledLogoWrapper>
+        <Image alt="logo" src={logo} />
+      </StyledLogoWrapper>
+    </StyledHeader>
+  );
 };
 
 export default Header;
--- src/components/Shared/Header/StyledHeader.ts
+++ src/components/Shared/Header/StyledHeader.ts
@@ -1,6 +1,41 @@
 import tw from "twin.macro";
 import styled from "styled-components";
 
-export const StyledHeader = styled.div`
-	${tw``}
+export const StyledHeader = styled.header`
+  ${tw` flex h-fit w-[60%] min-h-[60px] justify-between p-2 items-center`}
+  background: rgba(255, 255, 255, 0.2);
+  border-radius: 5px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.3);
+`;
+
+export const StyledProfile = styled.div`
+  ${tw`flex justify-center items-center gap-4`}
+  img {
+    width: 50px;
+    height: 50px;
+    border: 2px solid #4ab2f6;
+    border-radius: 100%;
+  }
+`;
+
+export const StyledInfoWrapper = styled.div`
+  ${tw`text-smokeWhite`}
+`;
+
+export const StyledNameTag = styled.h4`
+  ${tw`text-[12px]`}
+`;
+
+export const StyledTagline = styled.span`
+  ${tw`text-[10px] text-gray-400`}
+`;
+
+export const StyledLogoWrapper = styled.div`
+  ${tw``}
+  img {
+    width: 40px;
+  }
 `;
